78f289c17e0cfa11cb7dd14d108ca9c8772625a2
Marcus Vinicius Oliveira Stinghel <marcus.stinghel@extrabom.com.br>
2024-01-11 18:52:30 -0300

    Debbug 1.7 - data_frame_model

--- src/pkgs/csv/data_frame_model.py
+++ src/pkgs/csv/data_frame_model.py
@@ -23,6 +23,7 @@ class DataFrameModel:
 
     @classmethod
     def generate_new_line(cls, card: Task) -> list:
+        items = card.checklists[0].items if card.checklists else []
         return [
             card.name,
             card.status.name,
@@ -32,8 +33,8 @@ class DataFrameModel:
             datetime.fromtimestamp(int(card.start_date) / 1000) if card.start_date else '',
             datetime.fromtimestamp(int(card.due_date) / 1000) if card.due_date else '',
             datetime.fromtimestamp(int(card.date_closed) / 1000) if card.date_closed else '',
-            card.checklists[0].items[0].name if card.checklists and len(card.checklists[0].items) >= 1 else '',
-            card.checklists[0].items[1].name if card.checklists and len(card.checklists[0].items) >= 2 else '',
-            card.checklists[0].items[4].name if card.checklists and len(card.checklists[0].items) >= 5 else '',
-            card.checklists[0].items[5].name if card.checklists and len(card.checklists[0].items) >= 6 else ''
+            items[0].name if len(items) >= 1 else '',
+            items[1].name if len(items) >= 2 else '',
+            items[4].name if len(items) >= 5 else '',
+            items[5].name if len(items) >= 6 and card.checklists[0].items[5].name != 'SEM DATA' else ''
         ]
