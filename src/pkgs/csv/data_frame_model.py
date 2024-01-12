b517b81a77e4333e180919785fdcd4c1f152e796
Marcus Vinicius Oliveira Stinghel <marcus.stinghel@extrabom.com.br>
2024-01-12 11:32:58 -0300

    Debugg 1.11 - data_fame_model data format

--- src/pkgs/csv/data_frame_model.py
+++ src/pkgs/csv/data_frame_model.py
@@ -32,14 +32,14 @@ class DataFrameModel:
             card.assignees[0].username if card.assignees else default,
             card.tags[0].name if card.tags else default,
             card.tags[0].name if card.tags else default,
-            dt.fromtimestamp(int(card.start_date) / 1000) if card.start_date else default,
-            dt.fromtimestamp(int(card.due_date) / 1000) if card.due_date else default,
-            dt.fromtimestamp(int(card.date_closed) / 1000) if card.date_closed else default,
+            dt.fromtimestamp(int(card.start_date) / 1000).replace(microsecond=0) if card.start_date else default,
+            dt.fromtimestamp(int(card.due_date) / 1000).replace(microsecond=0) if card.due_date else default,
+            dt.fromtimestamp(int(card.date_closed) / 1000).replace(microsecond=0) if card.date_closed else default,
             items[0].name if len(items) >= 1 else default,
             items[1].name if len(items) >= 2 else default,
             items[4].name if len(items) >= 5 else default,
             (
-                dt.strptime(items[5].name, "%d/%m/%Y").strftime("%Y-%m-%d %H:%M:%S.%f")
+                dt.strptime(items[5].name, "%d/%m/%Y").strftime("%Y-%m-%d %H:%M:%S")
                 if len(items) >= 6
                 and re.match(r'\d{2}/\d{2}/\d{4}', items[5].name)
                 else default
