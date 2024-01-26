514d52236e0d66eb43fb18159301fc3be7f639e6
Marcus Vinicius Oliveira Stinghel <marcus.stinghel@extrabom.com.br>
2024-01-26 09:06:44 -0300

    Debugged - run files

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /local/do/projeto/cards_integration
 call venv\Scripts\activate
 python main.py
 deactivate
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /local/do/projeto/cards_integration
 source ./venv/Scripts/activate
 python main.py
 deactivate
