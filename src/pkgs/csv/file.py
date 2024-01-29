66814e017343ffbcea7fbcccd38327cf58036c27
Marcus Vinicius Oliveira Stinghel <marcus.stinghel@extrabom.com.br>
2024-01-29 13:21:13 -0300

    Refactored - minimal ajusts

--- src/pkgs/csv/file.py
+++ src/pkgs/csv/file.py
@@ -17,5 +17,8 @@ class File:
         data_frame_to_add.to_csv(self.__path, index=False, encoding='utf-8', mode='a', header=False)
 
     def __create_or_replace_file(self) -> None:
+        dir_path = os.path.dirname(self.__path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
         data_frame: pd.DataFrame = pd.DataFrame(columns=self.__header)
         data_frame.to_csv(self.__path, index=False, encoding='utf-8', mode='w')
